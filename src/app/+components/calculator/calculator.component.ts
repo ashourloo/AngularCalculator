9d8496d2f0589a93ab728dd9a73d43968fe09b3c
Abbas Ashourloo <abbas.ashourlou@gmail.com>
2024-07-11 11:41:32 +0330

    calculator finished

--- src/app/+components/calculator/calculator.component.ts
+++ src/app/+components/calculator/calculator.component.ts
@@ -8,5 +8,35 @@ import { Component } from '@angular/core';
   styleUrl: './calculator.component.css'
 })
 export class CalculatorComponent {
-
+  equal() {
+    const a=Number(this.previousValue);
+    const b=Number(this.currentValue);
+    let result=0;
+    switch (this.currentOperator) {
+      case '+':
+        result=a+b;
+        break;
+      case '-':
+        result=a-b;
+        break;
+      case 'x':
+        result=a*b;
+        break;
+      case '/':
+        result=a/b;
+        break;
+    }
+    this.currentValue=result.toString();
+  }
+  operator(o: string) {
+    this.currentOperator = o;
+    this.previousValue = this.currentValue;
+    this.currentValue = '0';
+  }
+  digit(d: string) {
+    this.currentValue = Number(this.currentValue + d).toString();
+  }
+  previousValue: string = '0';
+  currentOperator: string = '';
+  currentValue: string = '0';
 }
